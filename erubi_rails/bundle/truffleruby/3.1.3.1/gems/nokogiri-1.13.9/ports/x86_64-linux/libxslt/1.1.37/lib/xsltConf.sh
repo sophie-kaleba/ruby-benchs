4c49e53923fb54375fc782f080af258984d99754
Sophie Kaleba <sophie.kaleba@gmail.com>
2023-01-16 16:27:33 +0000

    add the standalone gems for ruby 3.1.3

---
+++ erubi_rails/bundle/truffleruby/3.1.3.1/gems/nokogiri-1.13.9/ports/x86_64-linux/libxslt/1.1.37/lib/xsltConf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/home/sopi/Documents/Side_projects/truffleruby/phd-bench/erubi_rails/bundle/truffleruby/3.1.3.1/gems/nokogiri-1.13.9/ports/x86_64-linux/libxslt/1.1.37/lib"
+XSLT_LIBS="-lxslt -L/home/sopi/Documents/Side_projects/truffleruby/phd-bench/erubi_rails/bundle/truffleruby/3.1.3.1/gems/nokogiri-1.13.9/ports/x86_64-linux/libxml2/2.10.3/lib -lxml2 "
+XSLT_PRIVATE_LIBS="-lm"
+XSLT_INCLUDEDIR="-I/home/sopi/Documents/Side_projects/truffleruby/phd-bench/erubi_rails/bundle/truffleruby/3.1.3.1/gems/nokogiri-1.13.9/ports/x86_64-linux/libxslt/1.1.37/include"
+MODULE_VERSION="xslt-1.1.37"
