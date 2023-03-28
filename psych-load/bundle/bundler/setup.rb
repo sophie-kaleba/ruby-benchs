edb202d2254ea1e477ae64409836ce903d3368e7
Sophie Kaleba <sophie.kaleba@gmail.com>
2023-03-28 11:11:38 +0100

    delete the standalone bundles

--- psych-load/bundle/bundler/setup.rb
+++
@@ -1,7 +0,0 @@
-require 'rbconfig'
-ruby_engine = RUBY_ENGINE
-ruby_version = RbConfig::CONFIG["ruby_version"]
-path = File.expand_path('..', __FILE__)
-$:.unshift "#{path}/"
-$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/extensions/x86_64-linux/3.1.3.1/psych-4.0.1"
-$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/psych-4.0.1/lib"
